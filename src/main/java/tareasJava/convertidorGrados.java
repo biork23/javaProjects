da2440818e839a09864d2f3362ceb6b93fe72ced
biork23 <ismaelbiork@gmail.com>
2023-06-22 01:08:47 -0600

    Se agrega convertidor de grados

--- src/main/java/tareasJava/convertidorGrados.java
+++ src/main/java/tareasJava/convertidorGrados.java
@@ -1,5 +1,22 @@
 package tareasJava;
 
+import java.util.Scanner;
+
 public class convertidorGrados {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+	float gradosC, gradosF;
+	System.out.println("Ingresa los grados Farenheit");
+	
+	gradosF = input.nextFloat();
+	
+	gradosC = (gradosF-32)* 5/9;
+	
+	System.out.println(gradosF+ " grados Farenheit son "+ gradosC + " grados Celsius.");
+	
+	}
+
+	
 
 }
